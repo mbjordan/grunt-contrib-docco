8838fdb867a4b4a4647bce7f10fe30140d1fda89
Matt Jordan <mbj755@gmail.com>
2015-07-23 15:54:13 -0400

    more abstraction

--- tasks/docco.js
+++ tasks/docco.js
@@ -1,6 +1,15 @@
 // MIT License
 // Copyright (c) 2015 Matt Jordan
 var docco = require('docco');
+var optionsArr = [
+    'layout',
+    'css',
+    'output',
+    'template',
+    'extension',
+    'languages',
+    'marked'
+];
 
 function processOptions(context) {
     return context.options({
@@ -9,15 +18,9 @@ function processOptions(context) {
 }
 
 function getOptions(context) {
-
-    // These have not *yet* been tested!
-    context.options.layout = context.options.layout || null;
-    context.options.css = context.options.css || null;
-    context.options.output = context.options.output || null;
-    context.options.template = context.options.template || null;
-    context.options.extension = context.options.extension || null;
-    context.options.languages = context.options.languages || null;
-    context.options.marked = context.options.marked || null;
+    for (var i = 0; i < optionsArr.length; i++) {
+        context.options[optionsArr[i]] = context.options[optionsArr[i]] || null;
+    }
 
     return processOptions(context);
 }
